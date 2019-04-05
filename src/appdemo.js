c82e5759b2653ae5c751a875530a526a2ccb33b5
William Yang <william.yang@zatech.com>
2019-04-05 21:27:04 +0800

    first job done

--- src/appdemo.js
+++ src/appdemo.js
@@ -7,12 +7,14 @@ new Vue({
     data:{
         currencies:{},
         amount:0,
+        computed_amount:0,
+        loading: false,
         from:'',
         to:''
     },
     mounted() {
         this.getCurrencies();
-        
+        this.computed_amount = this.amount
     },
     methods: {
         getCurrencies(){
@@ -22,11 +24,24 @@ new Vue({
                 this.currencies = JSON.parse(currencies)
                 return ;
             }
-            axios.get("https://free.currencyconverterapi.com/api/v6/currencies?apiKey=sample-api-key")
+            axios.get("https://free.currencyconverterapi.com/api/v6/currencies?apiKey=0c041c2c7d7aa0a329fd")
             .then(res => {
                 this.currencies = res.data.results
                 localStorage.setItem('currencies', JSON.stringify(res.data.results))
                 console.log(res)
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+        },
+        getRate(){
+            this.loading = true;
+            axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=${this.from}_${this.to}&compact=ultra&apiKey=0c041c2c7d7aa0a329fd`)
+            .then(res => {
+                console.log(res)
+                rate = res.data[`${this.from}_${this.to}`]
+                this.computed_amount = (this.amount * rate).toFixed(3)
+                this.loading =false;
             })
             .catch(err => {
                 console.error(err); 
@@ -36,6 +51,17 @@ new Vue({
     computed: {
         formattedCurrencies(){
             return Object.values(this.currencies)
+        },
+        disableButton(){
+            return this.amount ===0 || !this.amount || this.loading;
+        }
+    },
+    watch: {
+        from(){
+            this.computed_amount = 0;
+        },
+        to(){
+            this.computed_amount =0;
         }
     },
 });
