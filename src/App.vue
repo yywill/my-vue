24a6159e8ff3ee1d053a00a465adad5c86bc60ed
William Yang <william.yang@zatech.com>
2019-04-06 18:06:32 +0800

    shopping card task done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="container">
+   <div class="row">
+     <div class="col-md-7">
+       <div class="row">
+            <div :key="product.id" class="col-md-6" v-for="product in products">
+              <Product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"></Product>
+            </div>
+            
+        </div>
+
+        <div class="row">
+          
+        </div>
+        
+       </div>
+
+       <div class="col-md-5">
+         <cart v-on:clear="clear()" v-on:remove-item="removeItem($event)" :items="cart"></cart>
+       </div>
+        
+     </div>
+     <div class="col-md-5">
+     </div>
+     
+   </div>
 </template>
 
 <script>
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
+import Cart from '@/components/Cart.vue'
 
 export default {
   name: 'app',
+  components:{
+    Product,
+    Cart
+  },
+  data(){
+    return {
+      products,
+      cart:[]
+    }
+  },
+  methods:{
+    addToCart(prod){
+      this.cart.push(prod);
+    },
+    isInCart(prod){
+      const findone = this.cart.find(p=>p.id === prod.id);
+      if(findone){
+        return true;
+      }else{
+        return false;
+      }
+    },
+    removeItem(item){
+      this.cart = this.cart.filter(p=> p.id !== item.id);
+    },
+    clear(){
+      this.cart=[];
+    }
+  }
 }
 
 </script>
 
 <style>
-
+body {
+  background-color: #FBF8F3
+}
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <ul class="list-group">
+       <li :key="item.id" v-for="item in items" class="list-group-item">
+           {{item.title}} - ${{item.price}}
+           <button @click="$emit('remove-item', item)" class="btn badge badge-danger float-right">
+               remove from cart
+           </button>
+       </li>
+   </ul>
+   <div class="card my-5 p-3">
+       <h4 class="text-center">
+           total $ {{total}}
+       </h4>
+   </div>
+
+   <button :disabled="items.length === 0" @click="$emit('clear')" class="btn btn-info form-control">
+       Pay Now
+   </button>
+</div>
+   
+</template>
+<script>
+export default {
+    props:['items'],
+    computed:{
+        total(){
+            return this.items.reduce((acc, j) =>acc+ Number(j.price), 0);
+        }
+    }
+}
+</script>
